3e744db79beb52e8410018ab60043400a587a195
wilsom171 <53121506+wilsom171@users.noreply.github.com>
2021-08-08 13:06:29 -0500

    Update main.js

--- src/server/main.js
+++ src/server/main.js
@@ -7,12 +7,12 @@ const app = express()
 var server = http.createServer(app)
 const io = socket(server)
 
-app.set('port', process.env.port || 5000)
+app.set('port', process.env.PORT || 5000)
 app.use(express.static(path.join(__dirname, '../public')))
 
 require('./socket')(io)
 
-server = app.listen(process.env.PORT || 5000, () => {
+server.listen(process.env.PORT || 5000, () => {
   const port = server.address().port;
   console.log(`Express is working on port ${port}`);
 });
